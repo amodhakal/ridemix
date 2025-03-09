3df2d786f8d50d7d0e77966e4a2503a07b58dc51
Amodh Dhakal <adhakal2@ncsu.edu>
2025-03-09 17:56:39 -0400

    Created the landing page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Routes, Route } from "react-router";
+
 import "./style.css";
+import LandingPage from "./routes/LandingPage";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <h1 className="font-bold text-3xl">RideMix</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
