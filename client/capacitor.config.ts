c6569f14fec739be5af2956a5e775b45c8d4aaae
Amodh Dhakal <adhakal2@ncsu.edu>
2025-03-09 01:43:38 -0500

    Setup capacitor with icons

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.amodhakal.ridemix',
+  appName: 'RideMix',
+  webDir: 'dist'
+};
+
+export default config;
